88e4ba36c9377ab17e5bc09dcb612c13c5e7197f
AliFMDR <110685752+AliFMDR@users.noreply.github.com>
2023-06-09 23:15:01 +0700

    Add files via upload

---
+++ react1/tugas2/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/navbar";
+import Section1 from "./components/section1";
+import Footer from "./components/footer";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Section1 />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
